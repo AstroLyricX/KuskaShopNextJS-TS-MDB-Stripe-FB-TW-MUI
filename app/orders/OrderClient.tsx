8c81f21316c78f5a9ea86b56a65b7cf54f659503
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-11-28 10:29:28 -0500

    feat: create OrderClient.tsx

---
+++ app/orders/OrderClient.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import { Order, User } from "@prisma/client";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { formatPrice } from "@/utils/formatPrice";
+import { Heading } from "@/app/components/Heading";
+import { Status } from "@/app/components/Status";
+import {
+  MdAccessTimeFilled,
+  MdDeliveryDining,
+  MdDone,
+  MdRemoveRedEye,
+} from "react-icons/md";
+import { ActionBtn } from "@/app/components/ActionBtn";
+import { useRouter } from "next/navigation";
+import moment from "moment";
+
+interface OrderClientProps {
+  orders: ExtendedOrder[];
+}
+
+type ExtendedOrder = Order & {
+  user: User;
+};
+
+export const OrderClient: React.FC<OrderClientProps> = ({ orders }) => {
+  const router = useRouter();
+  let rows: any = [];
+
+  if (orders) {
+    rows = orders.map((order) => {
+      return {
+        id: order.id,
+        customer: order.user.name,
+        amount: formatPrice(order.amount / 100),
+        paymentStatus: order.status,
+        date: moment(order.createDate).fromNow(),
+        deliveryStatus: order.deliveryStatus,
+      };
+    });
+  }
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 200 },
+    { field: "customer", headerName: "Nombre del cliente", width: 200 },
+    {
+      field: "amount",
+      headerName: "Monto(USD)",
+      width: 120,
+      renderCell: (params) => {
+        return (
+          <div className="font-bold text-slate-800">{params.row.amount}</div>
+        );
+      },
+    },
+
+    {
+      field: "paymentStatus",
+      headerName: "Estado de pago",
+      width: 120,
+      renderCell: (params) => {
+        return (
+          <div>
+            {params.row.paymentStatus === "pending" ? (
+              <Status
+                text="Pendiente"
+                icon={MdAccessTimeFilled}
+                bg="bg-slate-200"
+                color="text-slate-700"
+              />
+            ) : params.row.paymentStatus === "complete" ? (
+              <Status
+                text="Completo"
+                icon={MdDone}
+                bg="bg-purple-200"
+                color="text-purple-700"
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+        );
+      },
+    },
+
+    {
+      field: "deliveryStatus",
+      headerName: "Estado de entrega",
+      width: 130,
+      renderCell: (params) => {
+        return (
+          <div>
+            {params.row.deliveryStatus === "pending" ? (
+              <Status
+                text="Pendiente"
+                icon={MdAccessTimeFilled}
+                bg="bg-slate-200"
+                color="text-slate-700"
+              />
+            ) : params.row.deliveryStatus === "dispatched" ? (
+              <Status
+                text="Enviado"
+                icon={MdDeliveryDining}
+                bg="bg-purple-200"
+                color="text-purple-700"
+              />
+            ) : params.row.deliveryStatus === "delivered" ? (
+              <Status
+                text="Entregado"
+                icon={MdDone}
+                bg="bg-green-200"
+                color="text-green-700"
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+        );
+      },
+    },
+    { field: "date", headerName: "Fecha", width: 100 },
+
+    {
+      field: "action",
+      headerName: "Acciones",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="flex justify-between gap-4 w-full">
+            <ActionBtn
+              icon={MdRemoveRedEye}
+              onClick={() => {
+                router.push(`/order/${params.row.id}`);
+              }}
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="max-w-[1150px] m-auto text-xl ">
+      <div className="mb-4 mt-8">
+        <Heading title="Administrar pedidos" center />
+      </div>
+      <div style={{ height: 600, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 9 },
+            },
+          }}
+          pageSizeOptions={[9, 20]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </div>
+    </div>
+  );
+};
