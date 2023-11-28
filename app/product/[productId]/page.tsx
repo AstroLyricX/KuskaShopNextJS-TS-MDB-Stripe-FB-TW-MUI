5deb22cac037e1354eb391e6aec4e020853b3212
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-11-28 16:01:27 -0500

    update page.tsx

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,23 +1,30 @@
 import { Container } from "@/app/components/Container";
 import { ProductDetails } from "./ProductDetails";
 import { ListRating } from "./ListRating";
-import { products } from "@/utils/products";
+import getProductById from "@/actions/getProductById";
+import { NullData } from "@/app/components/NullData";
+import { AddRating } from "./AddRating";
+import { getCurrenUser } from "@/actions/getCurrenUser";
 
 interface IParams {
   productId?: string;
 }
 
-const Product = ({ params }: { params: IParams }) => {
-  console.log("params:", params);
+const Product = async ({ params }: { params: IParams }) => {
+  const product = await getProductById(params);
+  const user = await getCurrenUser();
 
-  const product = products.find((item) => item.id === params.productId);
+  if (!product)
+    return (
+      <NullData title="¡Ups! El producto con el ID proporcionado no existe."></NullData>
+    );
 
   return (
     <div className="p-8">
       <Container>
         <ProductDetails product={product} />
         <div className="flex flex-col mt-20 gap-4">
-          <div>Agregar Calificación </div>
+          <AddRating product={product} user={user} />
           <ListRating product={product} />
         </div>
       </Container>
