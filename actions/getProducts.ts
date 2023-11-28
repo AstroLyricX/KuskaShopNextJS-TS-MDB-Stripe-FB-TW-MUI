590e15081f70c5596b684b06f665f5b2890632ba
Ever Ronaldo <ever.condori@upeu.edu.pe>
2023-11-28 16:00:13 -0500

    update getProducts.ts

--- actions/getProducts.ts
+++ actions/getProducts.ts
@@ -1,12 +1,10 @@
 import prisma from "@/libs/prismadb";
 
-// Define the parameters for the getProducts function
 export interface getProductsParams {
   category?: string | null;
   searchTerm?: string | null;
 }
 
-// Define the default export function getProducts
 export default async function getProducts(params: getProductsParams) {
   try {
     const { category, searchTerm } = params;
@@ -16,13 +14,11 @@ export default async function getProducts(params: getProductsParams) {
       searchString = "";
     }
 
-    // Initialize the query object with any provided category filter
     let query: any = {};
     if (category) {
       query.category = category;
     }
 
-    // Use Prisma to fetch products from the database based on the provided filters
     const products = await prisma.product.findMany({
       where: {
         ...query,
@@ -32,8 +28,6 @@ export default async function getProducts(params: getProductsParams) {
               contains: searchString,
               mode: "insensitive",
             },
-          },
-          {
             description: {
               contains: searchString,
               mode: "insensitive",
@@ -53,7 +47,6 @@ export default async function getProducts(params: getProductsParams) {
       },
     });
 
-    // Return the fetched products
     return products;
   } catch (error: any) {
     throw new Error(error);
